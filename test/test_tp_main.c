ce18ecda6636e155ad93c0d9aa30fc2e14e59c8f
MaximusZhou <zhougang009@gmail.com>
2014-12-10 21:34:17 +0800

    change test code

--- test/test_tp_main.c
+++ test/test_tp_main.c
@@ -1,8 +1,11 @@
 /*
-*gcc -g  -lpthread -I src/ -o test/test_tp_main  src/listed_blocking_queue.c src/threadpool.c test/test_tp_main.c
+* test_tp_main.c
+ * Author:MaximusZhou
+* gcc -g  -lpthread -I src/ -o test/test_tp_main  src/listed_blocking_queue.c src/threadpool.c test/test_tp_main.c
 */
 #define THREAD 5
 #define QUEUE  10
+#define MAX_THREAD  1000
 
 #include <stdio.h>
 #include <pthread.h>
@@ -11,7 +14,7 @@
 
 #include "threadpool.h"
 
-int tasks = 0, done = 0;
+int done = 0;
 pthread_mutex_t lock;
 
 void dummy_task(void *arg) {
@@ -23,27 +26,38 @@ void dummy_task(void *arg) {
 
 int main(int argc, char **argv)
 {
+	int tasks = 0;
+	add_task_t add_type;
 	threadpool *pool;
 
 	pthread_mutex_init(&lock, NULL);
 
 	assert((pool = threadpool_init(THREAD, QUEUE)) != NULL);
-	fprintf(stderr, "Pool started with %d threads and "
-			"queue size of %d\n", THREAD, QUEUE);
+	printf("Pool started with %d threads and queue size of %d\n", THREAD, QUEUE);
 
-	while(threadpool_add_task(pool, &dummy_task, NULL, ADD_NOBLOCKING) == 0) {
-		pthread_mutex_lock(&lock);
+	while(1) 
+	{
+		if(tasks % 2 == 0)
+			add_type = ADD_NOBLOCKING;
+		else
+			add_type = ADD_BLOCKING;
+
+		if (threadpool_add_task(pool, &dummy_task, NULL, add_type) != 0)
+			break;
+		
 		tasks++;
-		pthread_mutex_unlock(&lock);
-	}   
-
-	fprintf(stderr, "Added %d tasks\n", tasks);
+		if(tasks >= MAX_THREAD)
+			break;
+	} 
+	printf("Added %d tasks\n", tasks);
 
 	while((tasks / 2) > done) {
 		usleep(10);
-	}   
+	} 
 	threadpool_destroy(pool, 0); 
-	fprintf(stderr, "Did %d tasks\n", done);
+	printf("Did %d tasks\n", done);
+
+	pthread_mutex_destroy(&lock);
 
 	return 0;
 }
